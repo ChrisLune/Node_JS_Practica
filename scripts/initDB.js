153208c4f9cc37ca815eb9b8ebf678add181a789
Chris <christopher.lunae@gmail.com>
2024-11-08 20:03:31 -0600

    Views, Login

--- scripts/initDB.js
+++ scripts/initDB.js
@@ -16,11 +16,11 @@ async function initDB() {
   ]);
 
   const products = [
-    { name: 'Laptop', owner: users[0]._id, price: 500, image: 'image1.jpg', tags: ['work', 'Dell'] },
-    { name: 'Phone', owner: users[1]._id, price: 250, image: 'image2.jpg', tags: ['mobile', 'Iphone'] },
-    { name: 'Tablet', owner: users[2]._id, price: 300, image: 'image3.jpg', tags: ['portable', 'Samsung'] },
-    { name: 'Monitor', owner: users[3]._id, price: 200, image: 'image4.jpg', tags: ['work', 'LG'] },
-    { name: 'Keyboard', owner: users[4]._id, price: 50, image: 'image5.jpg', tags: ['accessory', 'Logitech'] }
+    { name: 'Laptop', owner: users[0]._id, price: 500, image: 'images/image1.jpg', tags: ['work', 'Dell'] },
+    { name: 'Phone', owner: users[1]._id, price: 250, image: 'images/image2.jpg', tags: ['mobile', 'Iphone'] },
+    { name: 'Tablet', owner: users[2]._id, price: 300, image: 'images/image3.jpg', tags: ['portable', 'Samsung'] },
+    { name: 'Monitor', owner: users[3]._id, price: 200, image: 'images/image4.jpg', tags: ['work', 'LG'] },
+    { name: 'Keyboard', owner: users[4]._id, price: 50, image: 'images/image5.jpg', tags: ['accessory', 'Logitech'] }
   ];
 
   await Product.insertMany(products);
